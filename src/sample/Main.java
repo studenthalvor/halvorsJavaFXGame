099aa745f962553ca65e47f8a79ef4669e20f4f8
jorgenbraseth <jorgen@spacemaker.ai>
2020-11-04 22:20:17 +0100

    canvas baby!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,10 @@
 package sample;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -8,42 +12,45 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
+
+    private GraphicsContext gc;
+    private Timeline gameLoop;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Drawing Operations Test");
         Group root = new Group();
         Canvas canvas = new Canvas(300, 250);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        this.gc = canvas.getGraphicsContext2D();
         drawShapes(gc);
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        startLoop();
+    }
+
+    private void startLoop() {
+        final Duration dur = Duration.millis(1000/60);
+        final KeyFrame frame = new KeyFrame(dur, evt -> {
+            Platform.runLater(()->drawShapes(this.gc));
+        });
+
+        this.gameLoop = new Timeline();
+        gameLoop.setCycleCount(Animation.INDEFINITE);
+        gameLoop.getKeyFrames().add(frame);
+        gameLoop.playFromStart();
     }
 
     private void drawShapes(GraphicsContext gc) {
+        gc.clearRect(0,0,300,250);
         gc.setFill(Color.GREEN);
         gc.setStroke(Color.BLUE);
         gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-        gc.fillOval(10, 60, 30, 30);
-        gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-        gc.fillPolygon(new double[]{10, 40, 10, 40},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                new double[]{210, 210, 240, 240}, 4);
+        gc.strokeLine(Math.random()*300, 10, 10, 40);
+
     }
 
     public static void main(String[] args) {
